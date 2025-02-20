9d87e49c3d66e9bae8e6121ad5f4ef8c1e680232
Abdun-Nur-Porag <abdunnurporag.bd@hotmail.com>
2025-02-20 12:28:02 +0600

    first release

---
+++ main/index.js
@@ -0,0 +1,11 @@
+console.warn("working")
+//creating app bar
+const app_bar=appBar().child([
+  appBarBody().child([
+    appBarLeft()
+   , 
+   appBarCenter()
+  , 
+  appBarRight()
+    ])
+  ]).add(".root")
